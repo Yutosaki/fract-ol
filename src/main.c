53b189d7dfaf8df591baf845fb6c287ef8c0a09f
Yutosaki <sasayu8190@gmail.com>
2025-02-11 21:22:21 +0900

    reafactor: 25 line

--- src/main.c
+++ src/main.c
@@ -6,10 +6,80 @@
 #include <stdlib.h>
 #include <ft_printf.h>
 
+int ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+double	ft_atof(const char *str)
+{
+	double	res;
+	double	sign;
+	int		i;
+	double	frac;
+	double	power;
+
+	res = 0.0;
+	sign = 1.0;
+	i = 0;
+	frac = 0.0;
+	power = 1.0;
+	if (str[i] == '-')
+	{ sign = -1.0; i++; }
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10.0 + (str[i++] - '0');
+	if (str[i] == '.')
+	{
+		i++;
+		while (str[i] >= '0' && str[i] <= '9')
+		{ frac = frac * 10.0 + (str[i] - '0'); power *= 10.0; i++; }
+		res = res + frac / power;
+	}
+	return (res * sign);
+}
+
+static void	print_usage(char *prog)
+{
+	ft_printf("Usage: %s [mandelbrot | julia <julia_c_re> <julia_c_im> | burning_ship]\n", prog);
+	exit(1);
+}
+
+static void	init_fractal_type(int argc, char **argv, t_all *all)
+{
+	if (argc < 2 || argc > 4)
+		print_usage(argv[0]);
+	if (ft_strcmp(argv[1], "mandelbrot") == 0)
+		all->env.type = MANDELBROT;
+	else if (ft_strcmp(argv[1], "julia") == 0)
+	{
+		all->env.type = JULIA;
+		if (argc == 4)
+		{
+			all->julia.c_re = ft_atof(argv[2]);
+			all->julia.c_im = ft_atof(argv[3]);
+		}
+		else
+		{
+			all->julia.c_re = -0.7;
+			all->julia.c_im = 0.27015;
+		}
+	}
+	else if (ft_strcmp(argv[1], "burning_ship") == 0)
+		all->env.type = BURNING_SHIP;
+	else
+		print_usage(argv[0]);
+}
+
 int	handle_key(int keycode, void *param)
 {
-	t_all *all = (t_all *)param;
+	t_all	*all;
 
+	all = (t_all *)param;
 	if (keycode == 65361) //left arrow
 		all->env.offset_re -= all->env.scale * 0.1;
 	else if (keycode == 65363) //right arrow
@@ -36,16 +106,13 @@ int	handle_destroy(void *param)
 	return (0);
 }
 
-static void	print_usage(char *prog)
-{
-	ft_printf("Usage: %s [mandelbrot | julia <julia_c_re> <julia_c_im> | burning_ship]\n", prog);
-	exit(1);
-}
-
 int	handle_mouse(int button, int x, int y, void *param)
 {
-	t_all *all = (t_all *)param;
-	double old_scale = all->env.scale;
+	t_all	*all;
+	double	old_scale;
+
+	all = (t_all *)param;
+	old_scale = all->env.scale;
 	if (button == 4)
 		all->env.scale *= 0.9;
 	else if (button == 5)
@@ -82,72 +149,11 @@ void set_env(t_all *all)
 	mlx_loop(all->env.mlx);
 }
 
-int ft_strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
-
-double ft_atof(const char *str)
-{
-	double res = 0.0;
-	double sign = 1.0;
-	int i = 0;
-	if (str[i] == '-')
-	{
-		sign = -1.0;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10.0 + (str[i] - '0');
-		i++;
-	}
-	if (str[i] == '.')
-	{
-		double f = 0.0;
-		double d = 10.0;
-		i++;
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			f = f + (str[i] - '0') / d;
-			d *= 10.0;
-			i++;
-		}
-		res += f;
-	}
-	return (res * sign);
-}
-
 int	main(int argc, char **argv)
 {
 	t_all	all;
 
-	if (argc < 2 || argc > 4)
-		print_usage(argv[0]);
-	if (ft_strcmp(argv[1], "mandelbrot") == 0)
-		all.env.type = MANDELBROT;
-	else if (ft_strcmp(argv[1], "julia") == 0)
-	{
-		all.env.type = JULIA;
-		if (argc == 4)
-		{
-			all.julia.c_re = ft_atof(argv[2]);
-			all.julia.c_im = ft_atof(argv[3]);
-		}
-		else
-		{
-			all.julia.c_re = -0.7;
-			all.julia.c_im = 0.27015;
-		}
-	}
-	else if (ft_strcmp(argv[1], "burning_ship") == 0)
-		all.env.type = BURNING_SHIP;
-	else
-		print_usage(argv[0]);
-	set_env(&all);	
+	init_fractal_type(argc, argv, &all);
+	set_env(&all);
+	return (0);
 }
