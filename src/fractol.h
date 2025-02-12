2a7546034945376df5c0b326b4da133a8b820133
Yutosaki <sasayu8190@gmail.com>
2025-02-12 13:24:24 +0900

    feat: add structure and definition of func

--- src/fractol.h
+++ src/fractol.h
@@ -1,6 +1,13 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# include <mlx.h>
+# include <stdlib.h>
+# include <math.h>
+# include <stdio.h>
+# include <string.h>
+# include <ft_printf.h>
+
 # define WIDTH 800
 # define HEIGHT 600
 # define MAX_ITER 100
@@ -18,8 +25,8 @@ typedef enum e_fractal
 
 typedef struct s_env
 {
-	void		*mlx;
-	void		*win;
+	void*		mlx;
+	void*		win;
 	t_fractal	type;
 	double		scale;
 	double		offset_re;
@@ -39,9 +46,30 @@ typedef struct s_all
 	t_julia	julia;
 }				t_all;
 
-void	render_fractal(t_all *all);
-void	render_mandelbrot(t_env *env);
-void	render_julia(t_env *env, t_julia *julia);
-void	render_burning_ship(t_env *env);
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}				t_point;
+
+static void put_pixel(char* data, t_point p, int color, int size_line, int bpp);
+
+static int calc_mandelbrot(double c_re, double c_im, t_env* env);
+static void render_mandelbrot_line(int y, t_env* env, char* data, int size_line, int bpp);
+static void render_mandelbrot_lines(t_env* env, char* data, int size_line, int bpp);
+void	render_mandelbrot(t_env* env);
+
+static int calc_julia(double zx, double zy, t_julia* julia, t_env* env);
+static void render_julia_line(int y, t_env* env, t_julia* julia, char* data, int size_line, int bpp);
+static void render_julia_lines(t_env* env, t_julia* julia, char* data, int size_line, int bpp);
+void	render_julia(t_env* env, t_julia* julia);
+
+static int calc_burning_ship(double c_re, double c_im, t_env* env);
+static void render_burning_ship_line(int y, t_env* env, char* data, int size_line, int bpp);
+static void render_burning_ship_lines(t_env* env, char* data, int size_line, int bpp);
+void	render_burning_ship(t_env* env);
+
+void	render_fractal(t_all* all);
+
 
 #endif // FRACTOL_H
